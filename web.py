211d409f591e47d20e18ea92e8eae5362d350346
michal.hvila <michal.hvila@gmail.com>
2018-09-06 17:42:19 +0200

    aaa

--- web.py
+++ web.py
@@ -1,19 +1,54 @@
-# imports
+# # imports
+# import tornado.ioloop
+# import tornado.web
+#
+# # write to screen
+# class MainHandler(tornado.web.RequestHandler):
+#     def get(self):
+#         self.render('web/index.html')
+#
+# # r"/" == root website address
+# #application = tornado.web.Application([(r"/", MainHandler)])
+# application = tornado.web.Application([
+#     (r'/', MainHandler),
+#     (r'/js/(.*)', tornado.web.StaticFileHandler, {'path': './static/js'}),
+#     (r'/css/(.*)', tornado.web.StaticFileHandler, {'path': './static/css'}),
+#     (r'/fonts/(.*)', tornado.web.StaticFileHandler, {'path': './static/fonts'}),
+#     (r'/images/(.*)', tornado.web.StaticFileHandler, {'path': './static/images'}),
+# ])
+#
+# # Start the server at port 7777
+# if __name__ == "__main__":
+#     PortNumber = str(7777)
+#     print('Server Running at http://localhost:' + PortNumber + '/')
+#     print('To close press ctrl + c')
+#     application.listen(PortNumber)
+#     tornado.ioloop.IOLoop.instance().start()
+
+
 import tornado.ioloop
 import tornado.web
+import time
 
-# write to screen
-class MainHandler(tornado.web.RequestHandler):
+class ItWorks(tornado.web.RequestHandler):
+    def get(self):
+        self.write("It Works!!")
+
+class Transcend(tornado.web.RequestHandler):
     def get(self):
         self.render('web/index.html')
 
-# r"/" == root website address
-application = tornado.web.Application([(r"/", MainHandler)])
 
-# Start the server at port 7777
+application = tornado.web.Application([
+    #(r"/", ItWorks),
+    (r"/", Transcend),      # ('web/index.html')
+    (r"/images/(.*)",tornado.web.StaticFileHandler, {"path": "./images"},),
+    (r"/images/(style.\css)",tornado.web.StaticFileHandler, {"path": "./images/css"},),
+],debug=True)
+
 if __name__ == "__main__":
     PortNumber = str(7777)
     print('Server Running at http://localhost:' + PortNumber + '/')
     print('To close press ctrl + c')
     application.listen(PortNumber)
-    tornado.ioloop.IOLoop.instance().start()
+tornado.ioloop.IOLoop.instance().start()
